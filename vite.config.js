73cda25a5e44d800bb5875faeb4c03b903c7c714
kkmkk1313 <khaled.d4rensics@gmail.com>
2026-02-11 11:14:10 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,16 +2,15 @@ import base44 from "@base44/vite-plugin"
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/SOS4DIGITAL/',   // 👈 IMPORTANT LINE ADDED
+  base: '/SOS4DIGITAL/',  // 👈 VERY IMPORTANT (must match repo name exactly)
 
-  logLevel: 'error', // Suppress warnings, only show errors
+  logLevel: 'error',
 
   plugins: [
     base44({
       legacySDKImports: process.env.BASE44_LEGACY_SDK_IMPORTS === 'true'
     }),
     react(),
-  ]
+  ],
 });
